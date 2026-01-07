5cbc86e646762da1504f2fbcab6c187fded8f8a4
Tom Bernard <sobhan.kh.62@gmail.com>
2026-01-07 16:34:37 -0500

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,8 +1,8 @@
 
 import { connect } from "cloudflare:sockets";
 
-let password = 'zxc123';
-let proxyIP = 'proxyip.us.cmliussss.net:443';
+let password = '';
+let proxyIP = '';
 //let sub = '';
 let subConverter = atob('U1VCQVBJLkNNTGl1c3Nzcy5uZXQ=');
 let subConfig = atob('aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL0FDTDRTU1IvQUNMNFNTUi9tYXN0ZXIvQ2xhc2gvY29uZmlnL0FDTDRTU1JfT25saW5lX01pbmlfTXVsdGlNb2RlLmluaQ==');
